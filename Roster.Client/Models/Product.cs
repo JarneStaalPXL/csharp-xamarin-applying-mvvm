18527dc45313ffa18c6bf7a31b85b0a6076c34ec
JarneStaalPXL <jarne.staal9@outlook.com>
2021-11-09 15:13:09 +0100

    completed module

---
+++ Roster.Client/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roster.Client.Models
+{
+    public class Product
+    {
+        public string Title { get; set; } = "Roster App";
+    }
+}
